1dc54f76e72390e28cf53bb17a2ab2bf73b96d0a
nurzhanm <nm4aq@virginia.edu>
2014-09-29 14:47:25 -0400

    message

---
+++ helper.py
@@ -0,0 +1,10 @@
+__author__="Nurzhan"
+# Name: Nurzhan Minavarov (nm4aq)
+# Class: CS 3240
+# File: hello.py
+
+def greeting(msg):
+    print msg
+
+if __name__ == '__main__' :
+    greeting("hello")
